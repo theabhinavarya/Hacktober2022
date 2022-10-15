14307721890665855f4b21dd686a36f845b8bd6d
sahilyeole <sahilyeole93@gmail.com>
2022-10-15 23:11:50 +0530

    added coins list

--- cryptocurrency tracker/src/components/CoinContainer.jsx
+++ cryptocurrency tracker/src/components/CoinContainer.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 
-const CoinContainer = () => {
+const CoinContainer = ({coinData}) => {
+  // console.log(coinData)
   return (
     <button className='hover:bg-[#0f1114] hover:cursor-pointer w-full h-[5.9rem] rounded-t flex  border-b border-[#a9a9a99f] '>
+      <div className='sm:flex hidden items-center'>
+      <img src={coinData.image} className="w-14 h-14 "/>
+      </div>
       <div className=' sm:w-[40%] w-[20%] h-full flex flex-col  justify-center items-start  px-5 '>
-        <h1 className=' sm:text-2xl text-[0.8rem]'>BTC</h1>
-        <h1 className='text-[#A9A9A9] sm:text-md  text-[0.8rem]'>Bitcoin</h1>
+        <h1 className=' sm:text-2xl text-[0.8rem]'>{coinData.symbol.toUpperCase()}</h1>
+        <h1 className='text-[#A9A9A9] sm:text-md  text-[0.8rem]'>{coinData.name}</h1>
       </div>
       <div className=' sm:w-[20%] w-[35%] h-full flex items-center just px-5 justify-center'>
-      <h1 className='sm:text-[0.95rem] text-[0.7rem]  '>$ 19,000.43</h1>
+      <h1 className='sm:text-[0.95rem] text-[0.7rem]  '>$ {coinData.current_price}</h1>
       </div>
       <div className='w-[20%] h-full flex items-center just px-5 justify-center'>
       <h1 className='sm:text-[0.95rem] text-[0.7rem] '>-2.03%</h1>
       </div>
       <div className=' sm:w-[20%] w-[35%] h-full flex items-center just px-5 justify-end'>
-      <h1 className='sm:text-[0.95rem] text-[0.7rem] '>$ 403,000M</h1>
+      <h1 className='sm:text-[0.95rem] text-[0.7rem] '>$ {coinData.market_cap}</h1>
       </div>
     </button>
   )
--- cryptocurrency tracker/src/components/CoinTable.jsx
+++ cryptocurrency tracker/src/components/CoinTable.jsx
@@ -6,6 +6,11 @@ import CoinContainer from './CoinContainer'
 const CoinTable = () => {
   const [coins, setCoins] = useState([])
   const [loading, setLoading] = useState(false)
+  const [search, setSearch] = useState('')
+  // useEffect(() => {
+  //   setSearch("eth")
+  // }, [])
+  
   const fetchcoins = async () =>{
     setLoading(true)
     const {data} = await axios.get(CoinList())
@@ -15,7 +20,14 @@ const CoinTable = () => {
   useEffect(() => {
    fetchcoins()
   }, [])
-  console.log(coins)
+  const handleSearch = () =>{
+    return coins.filter(
+      (coin)=> 
+      coin.name.toLowerCase().includes(search) ||
+      coin.symbol.toLowerCase().includes(search)
+     )
+  }
+  
   return (
     <div className='px-16'>
     <div className='mt-4 '>
@@ -23,29 +35,27 @@ const CoinTable = () => {
     </div>
     <input type="text" className='bg-primaryBg border border-[#777777] w-full mt-4 h-[3.3rem] rounded px-4 placeholder:text-[#b4b4b4] placeholder:italic outline-none focus:border-white' placeholder='Search For a Cryptocurrency'/>
     <div className='w-full h-14 bg-accent mt-6 rounded-t flex text-black font-semibold font-montserrat sm:text-sm text-[0.7rem] '>
-      <div className=' sm:w-[40%] w-[25%]  h-full flex items-center  px-5 '>
+      <div className=' sm:w-[40%] w-[25%]  h-full flex items-center  sm:pl-20 pl-6'>
         <h1>Coin</h1>
       </div>
-      <div className=' w-[20%] h-full flex items-center just px-5 justify-center'>
+      <div className=' w-[20%] h-full flex items-center  pl-12 justify-center'>
       <h1>Price</h1>
       </div>
-      <div className='w-[20%] h-full flex items-center just px-5 justify-center'>
+      <div className='w-[20%] h-full flex items-center  pl-9 justify-center'>
       <h1>24H Change</h1>
       </div>
-      <div className=' sm:w-[20%] w-[35%]  h-full flex items-center just px-5 justify-end'>
+      <div className=' sm:w-[20%] w-[35%]  h-full flex items-center  pr-7 justify-end'>
       <h1>Market Cap</h1>
       </div>
     </div>
       <div className=' h-[35rem] overflow-y-scroll '>
-      <CoinContainer/>
-      <CoinContainer/>
-      <CoinContainer/>
-      <CoinContainer/>
-      <CoinContainer/>
-      <CoinContainer/>
-      <CoinContainer/>
-      <CoinContainer/>
-      <CoinContainer/>
+        {
+          handleSearch().map((row)=>{
+            return(
+              <CoinContainer key={row.id} coinData={row} />
+            )
+          })
+        }
       </div>
     </div>
   )
